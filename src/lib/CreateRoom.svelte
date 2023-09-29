e9ba95a1783aea97864d8ed15150cca9e19505be
Santi <sontrax@gmail.com>
2023-09-29 12:07:18 +0200

    created placeholder landing

---
+++ src/lib/CreateRoom.svelte
@@ -0,0 +1,13 @@
+<script>
+    async function createRoom(){
+        let response = await fetch(`${location.protocol}//${location.hostname}:3000/rooms`, {method: "POST"});
+        let data = await response.text();
+        console.log(data);
+        location.href = `./room`;
+    }
+
+</script>
+
+<button on:click={createRoom}>
+    Create room
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import './styles.css';
+  import batLogo from './../assets/Edouard-Joseph-dAlton-bat.jpg'
+  import Counter from './../lib/Counter.svelte'
+  import CreateRoom from './../lib/CreateRoom.svelte';
+
+  let batDesciption = "An illustration by Edouard Joseph d’Alton of a bat's squeleton";
+</script>
+
+<main>
+  <div>
+    <a href="https://www.dmsguild.com/product/352670/Mordenkainen-in-Barovia" target="_blank" rel="noreferrer">
+      <img src={batLogo} class="logo" title={batDesciption} alt={batDesciption} />
+    </a>
+  </div>
+  <h1>Goblin 𓃛 Thermomancer</h1>
+
+  <div class="card">
+    <CreateRoom />
+  </div>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+  main {
+    flex: 1;
+    display: flex;
+    place-items: center;
+    flex-direction: column;
+	}
+  .logo {
+    height: 30em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em DarkRed);
+  }
+</style>
