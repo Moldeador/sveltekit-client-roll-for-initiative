8801bab6b8f230085a747b00c60509025ec71242
Santi <sontrax@gmail.com>
2023-10-03 17:29:28 +0200

    changed name of room for rooms

--- src/lib/CreateRoom.svelte
+++ src/lib/CreateRoom.svelte
@@ -2,7 +2,7 @@
     async function createRoom(){
         let response = await fetch(`${location.protocol}//${location.hostname}:3000/rooms`, {method: "POST"});
         let slug = await response.text();
-        location.href = `./room/${slug}`;
+        location.href = `./rooms/${slug}`;
     }
 
 </script>
