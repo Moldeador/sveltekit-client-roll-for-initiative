9fa2e293ed3fc376e08ccec04c3ab690eda6c2bb
Santi <sontrax@gmail.com>
2023-10-27 18:07:27 +0200

    finished style of modal

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -31,16 +31,71 @@
 
 <form on:submit|preventDefault={saveUserData} method={method}>
     <label for="characterName">Character name:</label>
-    <br>
     <input type="text" id="characterName" name="characterName" bind:value={characterName}>
-    <br>
+
     <label for="initiativeModifier">Initiative modifier:</label>
-    <br>
     <div class="slidecontainer">
-        <input type="range" min="-5" max="5" bind:value={initiativeModifier} class="slider" id="initiativeModifier" name="initiativeModifier">
-        <span id="sliderValue">{initiativeModifier}</span>
+        <input type="range" min="-10" max="10" bind:value={initiativeModifier} class="slider" id="initiativeModifier" name="initiativeModifier">
     </div>
-    <input type="submit" value="Submit">
+        <span id="sliderValue">{initiativeModifier}</span>
+    <button class="inverted">Submit</button>
 </form>
 
+<style>
+form{
+    display: flex;
+    flex-direction: column;
+    margin: 5px;
+    justify-content: center;
+    align-items: center;
+}
+button{
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+input[type=text] {
+  font-family: 'Sen Variable', sans-serif;
+  width: 100%;
+  padding: 12px 20px;
+  margin-top: 5px;
+  margin-bottom: 20px;
 
+  box-sizing: border-box;
+  border: 3px solid #ccc;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+}
+
+input[type=text]:focus {
+  border: 3px solid #555;
+}
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;  
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%; 
+  background: #323232;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #323232;
+  cursor: pointer;
+}
+</style>
